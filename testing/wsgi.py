3e2368f12c568565e891f94ff0c469d919956cd0
Shantilal <shantilal13@gmail.com>
2021-03-08 12:42:24 +0530

    extra setting for static files

--- testing/wsgi.py
+++ testing/wsgi.py
@@ -7,20 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing.settings')
-
-# application = get_wsgi_application()
-
-
 import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testing.settings")
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+
